b12aca680985e57143ec2facfe79dbba88a120c8
Catherine Marston <catherine@thesnowflakeapp.com>
2016-06-07 16:21:22 -0600

    Upload files

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
 
-	$(window).scroll(function() {
-	    var scroll = $(window).scrollTop();
-	    if (scroll > 1) {
-	      $('body').addClass('scrolled');
-	    } else if (scroll < 1) {
-	      $('body').removeClass('scrolled');
-	    }
-  	});
-
+    var scroll_pos = 0;
+    var animation_begin_pos = 0; //where you want the animation to begin
+    var animation_end_pos = 1000; //where you want the animation to stop
+    var beginning_color = new $.Color( 'rgb(210,50,98)' ); //we can set this here, but it'd probably be better to get it from the CSS; for the example we're setting it here.
+    var ending_color = new $.Color( 'rgb(0,197,209)' ); ;//what color we want to use in the end
+    $(document).scroll(function() {
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos >= animation_begin_pos && scroll_pos <= animation_end_pos ) {
+           // console.log( 'scrolling and animating' );
+            //we want to calculate the relevant transitional rgb value
+            var percentScrolled = scroll_pos / ( animation_end_pos - animation_begin_pos );
+            var newRed = beginning_color.red() + ( ( ending_color.red() - beginning_color.red() ) * percentScrolled );
+            var newGreen = beginning_color.green() + ( ( ending_color.green() - beginning_color.green() ) * percentScrolled );
+            var newBlue = beginning_color.blue() + ( ( ending_color.blue() - beginning_color.blue() ) * percentScrolled );
+            var newColor = new $.Color( newRed, newGreen, newBlue );
+            //console.log( newColor.red(), newColor.green(), newColor.blue() );
+            $('.menu').animate({ backgroundColor: newColor }, 0);
+        } else if ( scroll_pos > animation_end_pos ) {
+             $('.menu').animate({ backgroundColor: ending_color }, 0);
+        } else if ( scroll_pos < animation_begin_pos ) {
+             $('.menu').animate({ backgroundColor: beginning_color }, 0);
+        } else { }
 });
-
-
-var target = $('.foreground');
-var targetHeight = target.outerHeight();
-
-$(document).scroll(function(e){
-    var scrollPercent = (targetHeight - window.scrollY) / targetHeight;
-    if(scrollPercent >= 0){
-        target.css('opacity', scrollPercent);
-    }
 });
